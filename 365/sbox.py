661d14502f2d001209736c8b6d8b3ae0e295b2ae
3ri5 <45490952+3ri5@users.noreply.github.com>
2022-03-24 11:44:20 -0700

    Create sbox.py

---
+++ 365/sbox.py
@@ -0,0 +1,14 @@
+#Your first state is d00245c36624d90305285c5024f648b8
+#Your second state is f715501eb68cd8862cb905761616af5b
+#Your third state is 986470c3089865660250922e68bdda13
+
+p1 = "d00245c36624d90305285c5024f648b8"
+p2 = "f715501eb68cd8862cb905761616af5b"
+p3 = "986470c3089865660250922e68bdda13"
+arr1 = []
+
+i=2
+while(i<len(arr1)):
+    for i in p1:
+        arr1.append(p1[i:2])
+        print(arr1)
